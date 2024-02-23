6011a5f869b1f1701a0d99f73ecb39afe82f98ef
Spencer Young <spencerwyoung@outlook.com>
2024-02-22 18:39:54 -0800

    somewhat simplify the TextSegment definition

--- mintyml/tests/utils.rs
+++ mintyml/tests/utils.rs
@@ -1,10 +1,14 @@
 use mintyml::OutputConfig;
 
+#[track_caller]
 pub fn convert_unwrap(src: impl AsRef<str>, cfg: impl Into<Option<OutputConfig>>) -> String {
     let res = mintyml::convert(src.as_ref(), cfg.into().unwrap_or_default());
     #[cfg(feature = "std")]
     {
-        res.unwrap_or_else(|e| panic!("{e}"))
+        match res {
+            Ok(x) => x,
+            Err(e) => panic!("{e}"),
+        }
     }
     #[cfg(not(feature = "std"))]
     {
