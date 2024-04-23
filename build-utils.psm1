31c1d228f1d2737394ce3a65e82efdc35c749a20
Spencer Young <spencerwyoung@outlook.com>
2024-04-22 20:32:57 -0700

    auto-publish

---
+++ build-utils.psm1
@@ -0,0 +1,250 @@
+using namespace System.Collections.Generic
+$ErrorActionPreference = 'Stop'
+$WSRoot = $PSScriptRoot
+
+function Test-ExitCode {
+    $command = $args[0]
+    $argList = $args[1..$args.Length]
+    if ($command) {
+        $global:LASTEXITCODE = 0
+        try {
+            & $command @argList
+            if ($LASTEXITCODE -ne 0) {
+                Write-Error "$command failed with exit code $LASTEXITCODE"
+            }
+        }
+        catch {
+            Write-Error $_
+        }
+    }
+    else {
+        if ($LASTEXITCODE -ne 0) {
+            Write-Error "command failed with exit code $LASTEXITCODE"
+        }
+    }
+}
+
+function Use-Location(
+    [string] $Path = $null,
+    [scriptblock] $Body
+) {
+    Push-Location -Path:$Path -StackName "Use-Location"
+    try {
+        $input | & $Body
+    }
+    finally {
+        Pop-Location -StackName "Use-Location"
+    }
+}
+
+function Get-Version {
+    Get-Content "$WSRoot/Cargo.toml" -Raw `
+    | Select-String '(?m)^\s*version\s*=\s*"(?<version>.*?)"\s*$' `
+    | ForEach-Object { $_.Matches[0].Groups['version'].Value }
+}
+
+function Get-VersionIncrement(
+    [ValidateNotNullOrWhiteSpace()][string] $Version
+) {
+    $parts = $Version -split '\.'
+    $parts[-1] = ([int]$parts[-1]) + 1
+    $parts -join '.'
+}
+
+function Set-Version([string] $Version) {
+    $src = (Get-Content "$WSRoot/Cargo.toml" -Raw)
+    $out = $src -replace '(?m)(?<=^\s*version\s*=\s*").*?(?="\s*$)', $Version
+
+    $out > "$WSRoot/Cargo.toml"
+}
+
+$packageNames = 'mintyml', 'minty-cli', 'minty-wasm'
+
+class WSState {
+    [string] $OldTag
+    [string] $NewVersion
+    [List[string]] $Packages = @()
+    [string[]] $Targets = @()
+}
+
+function Get-NewVersion([WSState] $State) {
+    $oldVersion = Get-Version
+    $State.OldTag = git show-ref -s --tags "v$oldVersion"
+
+    if ($State.OldTag) {
+        $State.NewVersion = Get-VersionIncrement
+        Write-Host (
+            "::notice title=Version::Tag 'v$oldVersion' already exists; " +
+            "Incrementing to v$($State.NewVersion)"
+        )
+    }
+    else {
+        Write-Host "::notice title=Version::Using 'v$oldVersion' from manifest file"
+        $State.NewVersion = $oldVersion
+    }
+    $State.NewVersion
+}
+
+function Get-OutOfDatePackages([WSState] $State) {
+    & {
+        if (-not $State.OldTag) {
+            return $packageNames
+        }
+
+        $changes = git diff --name-only $State.OldTag HEAD
+
+        if ($changes -like 'mintyml/*') {
+            # Update all packages if the core package changed
+            return $packageNames
+        }
+
+        if ($changes -like 'minty-cli/*') {
+            'minty-cli'
+        }
+
+        if ($changes -like 'minty-wasm/*') {
+            'minty-wasm'
+        }
+
+    } | ForEach-Object { $State.Packages.Add($_) }
+}
+
+function Build-NodeManifest([WSState] $State = $null) {
+    $version = $State.NewVersion ?? (Get-Version)
+    $src = Get-Content "$WSRoot/minty-wasm/package.template.json" -Raw
+    $out = $src -creplace '\{\{VERSION\}\}', $version
+    $out > "$WSRoot/minty-wasm/package.json"
+}
+
+function Update-Version([WSState] $State) {
+    $version = Get-NewVersion $State
+    $packages = Get-OutOfDatePackages $State
+
+    if (-not $packages) {
+        return
+    }
+
+    if ($State.OldTag) {
+        Set-Version $version
+    }
+
+    cargo update --workspace --offline
+}
+
+function Publish-Packages([switch] $Publish) {
+    $State = [WSState]::new()
+    Update-Version $State
+
+    if (-not $State.Packages) {
+        Write-Host "::notice title=No packages to publish::"
+        return
+    }
+
+    Write-Host "::notice title=Packages will be published::$($State.Packages -join ', ')"
+
+    $dryRun = $Publish ? @() : @('--dry-run')
+    $successCount = 0
+
+    if ("mintyml" -in $State.Packages) {
+        try {
+            Test-ExitCode cargo publish -q -p mintyml @dryRun --allow-dirty
+            $successCount += 1
+        }
+        catch {
+            Write-Host "::error::mintyml failed to publish"
+        }
+    }
+
+    if ("minty-cli" -in $State.Packages) {
+        try {
+            Test-ExitCode cargo publish -q -p mintyml-cli @dryRun --allow-dirty
+            $successCount += 1
+        }
+        catch {
+            Write-Host "::error::minty-cli failed to publish"
+        }
+    }
+
+    if ("minty-wasm" -in $State.Packages) {
+        just -f "$WSRoot/justfile" build-node
+        Push-Location "$WSRoot/minty-wasm"
+        try {
+            Build-NodeManifest $State
+            Test-ExitCode npm publish @dryRun
+            $successCount += 1
+        }
+        catch {
+            Write-Host "::error::minty-wasm failed to publish"
+        }
+        finally {
+            Pop-Location
+        }
+    }
+
+    if ($successCount -eq 0) {
+        return
+    }
+
+    Sync-Changes $State
+    Build-Release $State
+    if ($Publish) {
+        Publish-Release $State
+    }
+}
+
+function Sync-Changes([WSState] $State, [switch] $Publish) {
+    $tagName = "v$($State.NewVersion)"
+    $dryRun = $Publish ? @() : @('--dry-run')
+
+    Test-ExitCode git add .
+    Test-ExitCode git commit -m "Increment to $tagName"
+    Test-ExitCode git pull --rebase --strategy-option=theirs
+    Test-ExitCode git tag --force $tagName -m ""
+    Test-ExitCode git push @dryRun
+    Test-ExitCode git push @dryRun origin "refs/tags/$tagName"
+}
+
+function Build-Release([WSState] $State) {
+    if ("minty-cli" -notin $State.Packages) {
+        return
+    }
+
+    $State.Targets = Get-Content "$WSRoot/release-targets.txt" `
+    | ForEach-Object Trim `
+    | Where-Object { $_ -and $_ -notlike '#*' }
+
+    $version = $State.NewVersion
+
+    foreach ($target in $State.Targets) {
+        try {
+            Test-ExitCode rustup target add $target
+            Test-ExitCode cross build -q --release -p mintyml-cli --target $target
+
+            $file = Get-ChildItem `
+                "$WSRoot/target/$target/release/mintyml-cli*" `
+                -File -Include 'mintyml-cli','mintyml-cli.exe'
+
+            $outName = "mintyml-cli-$target-v$version"
+
+            New-Item -ItemType Directory -Force $outName
+
+            Test-ExitCode tar -czf "$WSRoot/target-release/$outName.tgz" `
+                -C "$WSRoot/target-release/" $outName
+        }
+        catch {
+            Write-Host "::error::Target '$target' Failed: $_"
+        }
+    }
+}
+
+function Publish-Release([WSState] $State) {
+    if ("minty-cli" -notin $State.Packages) {
+        return
+    }
+
+    $tagName = "v$($State.NewVersion)"
+    $assets = Get-ChildItem "target-release/*.tgz" | ForEach-Object FullName
+    Test-ExitCode gh release create $tagName @assets
+}
+
+Export-ModuleMember Publish-Packages, Build-NodeManifest
--- justfile
+++ justfile
@@ -3,47 +3,66 @@ set positional-arguments
 @default:
     just --list
 
+install: install-node install-web-demo
+    cargo binstall cargo-edit
+
 test-core:
-    cargo test --manifest-path mintyml/Cargo.toml --verbose
+    cargo test -q --manifest-path mintyml/Cargo.toml
 
 test-cli:
-    cargo test --manifest-path minty-cli/Cargo.toml --verbose
+    cargo test -q --manifest-path minty-cli/Cargo.toml
 
 @test-node: install-node (build-node-wasm "node")
-    just exec minty-wasm test-exec
+    just do minty-wasm test-exec
 
 @test: test-core test-cli test-node
 
 @build-node-wasm VARIANT:
-    just exec minty-wasm build-wasm-{{ VARIANT }}
+    just do minty-wasm build-wasm-{{ VARIANT }}
 
-@install-node:
-    just exec minty-wasm install
+install-node:
+    #!/usr/bin/env pwsh
+    $ErrorActionPreference = 'Stop'
+    Import-Module ./build-utils.psm1
+    Build-NodeManifest
+    just do minty-wasm install
 
 @build-node-tsc: install-node
-    just exec minty-wasm build-tsc
+    just do minty-wasm build-tsc
 
 @build-node: install-node (build-node-wasm "web") (build-node-wasm "node") build-node-tsc
 
 @publish-node: build-node test-node
-    just exec minty-wasm publish-exec
+    just do minty-wasm publish-exec
 
 @install-web-demo:
-    just exec web-demo install
+    just do web-demo install
 
 @serve-web-demo: install-web-demo && (build-node-wasm "web") build-node-tsc
-    just exec web-demo serve &
+    just do web-demo serve &
 
 @build-web-demo: (build-node-wasm "web") build-node-tsc install-web-demo
-    just exec web-demo webpack
+    just do web-demo webpack
 
 build-cli:
-    cargo build --release --manifest-path minty-cli/Cargo.toml
+    cargo build -q --release --manifest-path minty-cli/Cargo.toml
+
+publish-packages:
+    #!/usr/bin/env pwsh
+    $ErrorActionPreference = 'Stop'
+    Import-Module ./build-utils.psm1
+    Publish-Packages -Publish
 
 act *ARGS:
-    #!sh
+    #!/usr/bin/env sh
     repo=$(git remote get-url origin | sed -E 's:^.*\W(\w+/\w+)\.git$:\1:')
-    gh act --env GH_TOKEN=$(gh auth token) --pull=false --rebuild=false --local-repository $repo=./ "$@"
+    gh act \
+        --env GH_TOKEN=$(gh auth token) \
+        --env CROSS_CONTAINER_IN_CONTAINER=true \
+        --local-repository $repo=./ \
+        --privileged \
+        "$@"
 
-@exec DIR CMD *ARGS:
+
+@do DIR CMD *ARGS:
     shift 2; just {{DIR}}/{{CMD}} "$@"
